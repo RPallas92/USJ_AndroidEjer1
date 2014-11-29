6be6ad44c98eec5458a804725f51b45b343b5189
Ricardo Pallás <rpallas92@gmail.com>
2014-11-29 12:35:55 +0100

    Pantallas mejoradas

--- app/src/main/java/es/rpallas/usjandroidejer1/CIFActivity.java
+++ app/src/main/java/es/rpallas/usjandroidejer1/CIFActivity.java
@@ -2,16 +2,119 @@ package es.rpallas.usjandroidejer1;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 
 public class CIFActivity extends Activity {
+
+
+    private RadioGroup radioGroup;
+    private boolean esEmpresa = true;
+
+    private TextView cifText;
+    private EditText cifEditText;
+    private TextView dniText;
+    private EditText dniEditText;
+
+    private Button inciarDeposito;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cif);
+
+        cifText = (TextView) findViewById(R.id.cifText);
+        cifEditText = (EditText) findViewById(R.id.CIFEditText);
+        dniText = (TextView) findViewById(R.id.DNIText);
+        dniEditText = (EditText) findViewById(R.id.DNIEditText);
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+
+        inciarDeposito = (Button) findViewById(R.id.datosWebButton);
+        inciarDeposito.setEnabled(false);
+
+        dniText.setVisibility(View.GONE);
+        dniEditText.setVisibility(View.GONE);
+
+
+
+        int checkedRadioButtonID = radioGroup.getCheckedRadioButtonId();
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            public void onCheckedChanged(RadioGroup arg0, int id) {
+                switch (id) {
+                    case R.id.empresaRadioButton:
+                        esEmpresa = true;
+                        dniEditText.setText("");
+                        dniText.setVisibility(View.GONE);
+                        dniEditText.setVisibility(View.GONE);
+                        cifEditText.setVisibility(View.VISIBLE);
+                        cifText.setVisibility(View.VISIBLE);
+
+                        break;
+                    case R.id.ciudadanoRadioButton:
+                        esEmpresa = false;
+                        cifEditText.setText("");
+                        dniText.setVisibility(View.VISIBLE);
+                        dniEditText.setVisibility(View.VISIBLE);
+                        cifEditText.setVisibility(View.GONE);
+                        cifText.setVisibility(View.GONE);
+
+                        break;
+
+                    default:
+
+                        break;
+                }
+            }
+        });
+
+
+        TextWatcher textoHaCambiado = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                activarBotonSiRellenado();
+            }
+        };
+
+        cifEditText.addTextChangedListener(textoHaCambiado);
+        dniEditText.addTextChangedListener(textoHaCambiado);
+
+    }
+
+
+
+
+
+    private void activarBotonSiRellenado() {
+
+        boolean cifRellenado = cifEditText.getText().toString().length() > 1;
+        boolean dniRellenado = dniEditText.getText().toString().length() > 1;
+
+        if (cifRellenado || dniRellenado) {
+            inciarDeposito.setEnabled(true);
+        } else {
+            inciarDeposito.setEnabled(false);
+        }
     }
 
 
--- app/src/main/java/es/rpallas/usjandroidejer1/LoginActivity.java
+++ app/src/main/java/es/rpallas/usjandroidejer1/LoginActivity.java
@@ -1,12 +1,16 @@
 package es.rpallas.usjandroidejer1;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class LoginActivity extends Activity {
@@ -24,14 +28,54 @@ public class LoginActivity extends Activity {
         passwordText = (TextView) findViewById(R.id.loginPasswordText);
 
         enterButton = (Button) findViewById(R.id.loginEnterButton);
+        enterButton.setEnabled(false);
         enterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (userText.equals(Ejercico1Application.USER) && passwordText.equals(Ejercico1Application.PASSWORD)){
-                    
+                if (((userText.getText().toString()).equals(Ejercico1Application.USER)) && ((passwordText.getText().toString()).equals(Ejercico1Application.PASSWORD))){
+                    Toast.makeText(LoginActivity.this, "Acceso correcto", Toast.LENGTH_SHORT).show();
+                    Intent lanzarCIFActivity = new Intent(LoginActivity.this, CIFActivity.class);
+                    lanzarCIFActivity.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
+                            Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(lanzarCIFActivity);
+                } else {
+                    Toast.makeText(LoginActivity.this, "Acceso incorrecto", Toast.LENGTH_SHORT).show();
                 }
             }
         });
+
+        TextWatcher textoHaCambiado = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                activarBotonSiRellenado();
+            }
+        };
+
+        userText.addTextChangedListener(textoHaCambiado);
+        passwordText.addTextChangedListener(textoHaCambiado);
+    }
+
+
+    private void activarBotonSiRellenado() {
+
+        boolean userRellenado = userText.getText().toString().length()>1;
+        boolean passwordRellenado = passwordText.getText().toString().length()>1;
+
+        if (userRellenado && passwordRellenado) {
+            enterButton.setEnabled(true);
+        } else {
+            enterButton.setEnabled(false);
+        }
     }
 
 
